89483ae01c9ba9173d5d905e170d6927a34ecd0f
Simon Hutchinson <simonhutc@gmail.com>
2021-06-13 22:48:33 +1000

    Next Commit

--- Blocks/ContentView.swift
+++ Blocks/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 0
     var body: some View {
-        TabView(selection: .constant(1)) {
-            Text("Tab Content 1").tabItem { Text("News") }.tag(1)
-            Text("Tab Content 2").tabItem { Text("Products") }.tag(2)
-            Text("Tab Content 3").tabItem {
-                Text("Chat") }.tag(3)
+        TabView(selection: $selection) {
+            NavigationView {
+                Text("Tab Content 1")
+                    .navigationTitle("News")
             }
+            .tabItem { Text("News")}.tag(1)
+            NavigationView {
+                Text("Tab Content 2")
+                    .navigationTitle("Products")
+            }
+            .tabItem { Text("Products")}.tag(2)
+            NavigationView {
+                Text("Tab Content 3")
+                    .navigationTitle("Chat")
+            }
+            .tabItem { Text("Chat").tag(3)}
         }
+    
+    }
+    
     }
 
 struct ContentView_Previews: PreviewProvider {
