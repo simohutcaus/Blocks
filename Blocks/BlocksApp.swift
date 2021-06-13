322e96b9b2b85f549f325cf2deda7fcc5cdcbda5
Simon Hutchinson <simonhutc@gmail.com>
2021-06-13 21:40:36 +1000

    First

---
+++ Blocks/BlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlocksApp.swift
+//  Blocks
+//
+//  Created by admin on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct BlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Blocks
+//
+//  Created by admin on 6/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("News")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
