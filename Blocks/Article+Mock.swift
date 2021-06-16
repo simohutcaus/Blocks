777cb114798be58f0816ca759bd3f564a4289eae
Simon Hutchinson <simonhutc@gmail.com>
2021-06-16 21:23:02 +1000

    article

---
+++ Blocks/Article+Mock.swift
@@ -0,0 +1,29 @@
+//
+//  Article+Mock.swift
+//  Blocks
+//
+//  Created by admin on 6/16/21.
+//
+
+import Foundation
+
+
+extension Article {
+    
+    static let blockChain: Self = .init(
+        title: "Lego Block Chain",
+        date: Date(),
+        detail: "Add your own Lego block to the chain of transactions to guarantee security by trusting a whole crowd of people you've never met. What can possibly go wrong?",
+        smallImageName: "blockCircle",
+        largeImageName: "chain"
+    )
+    
+    static let airBlock: Self = .init(
+        title: "Air Block & Block",
+        date: Date(),
+        detail: "Rent out your Lego house for extra dollars. Create an extra revenue stream for your family by renting out your unused rooms.",
+        smallImageName: "houseSimple",
+        largeImageName: "houseInterior"
+    )
+    
+}
---
+++ Blocks/Article.swift
@@ -0,0 +1,17 @@
+//
+//  Article.swift
+//  Blocks
+//
+//  Created by admin on 6/16/21.
+//
+
+import Foundation
+
+struct Article {
+    let title: String
+    let date: Date
+    let detail: String
+    let smallImageName: String
+    let largeImageName: String
+}
+
